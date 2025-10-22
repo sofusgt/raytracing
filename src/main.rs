8232af0847bd37f244c506227cbe65406919fdef
Sofus Tornbjerg <sofus.tornbjerg@gmail.com>
2025-10-22 09:18:51 +0200

    Create PNG image rather than PPM

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ fn main() {
     let image_width = 256;
     let image_height = 256;
 
-    print!("P3\n{image_width} {image_height}\n255\n");
-    for j in 0..image_height {
-        for i in 0..image_width {
-            let r = i as f32 / (image_width as f32 - 1.0);
-            let g = j as f32 / (image_height as f32 - 1.0);
-            let b = 0.0;
+    let mut image = image::ImageBuffer::new(image_width, image_height);
+    for (x, y, pixel) in image.enumerate_pixels_mut() {
+        let r = x as f32 / (image_width as f32 - 1.0);
+        let g = y as f32 / (image_height as f32 - 1.0);
+        let b = 0.0;
 
-            let r = (255.999 * r) as i32;
-            let g = (255.999 * g) as i32;
-            let b = (255.999 * b) as i32;
+        let r = (255.999 * r) as u8;
+        let g = (255.999 * g) as u8;
+        let b = (255.999 * b) as u8;
 
-            print!("{r} {g} {b}\n");
-        }
+        *pixel = image::Rgb([r, g, b]);
     }
+
+    image.save("image.png").unwrap();
 }
